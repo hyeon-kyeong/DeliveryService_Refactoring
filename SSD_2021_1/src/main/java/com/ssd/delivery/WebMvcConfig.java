5d892d63f034b7d1f28e6217f35e6063d7cfd69c
khk99 <khk99@LAPTOP-0PU2A6A6>
2022-04-25 17:38:36 +0900

    SignonInterceptor 주석처리, 실제 SignonInterceptor 삭제 필요

--- SSD_2021_1/src/main/java/com/ssd/delivery/WebMvcConfig.java
+++ SSD_2021_1/src/main/java/com/ssd/delivery/WebMvcConfig.java
@@ -1,19 +1,19 @@
 package com.ssd.delivery;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {  
 
-	@Autowired
-	@Qualifier(value = "signonInterceptor") 
-	private HandlerInterceptor interceptor;
+//	@Autowired
+//	@Qualifier(value = "signonInterceptor") 
+//	private HandlerInterceptor interceptor;
  
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -69,11 +69,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	}
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(interceptor)
-				.addPathPatterns("/shop/editAccount.do", "/shop/listOrders.do",
-					"/shop/viewOrder.do", "/shop/newOrder.do");
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(interceptor)
+//				.addPathPatterns("/shop/editAccount.do", "/shop/listOrders.do",
+//					"/shop/viewOrder.do", "/shop/newOrder.do");
+//	}
 
 }
